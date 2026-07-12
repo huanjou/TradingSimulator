d001803cc3cf220d96d3febf0c27586a618f060e
ABOBUSBUS <tshkhanov@mail.ru>
2026-07-12 16:20:19 +0300

    fix(frontend): remove next.config.js rewrites to prevent local API resolution issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'standalone',
-  reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://nginx/api/:path*', // proxy to nginx in docker network
-      },
-    ];
-  },
 };
+
 module.exports = nextConfig;
