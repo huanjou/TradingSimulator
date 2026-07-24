a838479772459cf5b2132278a7be82e6cca08d6e
ABOBUSBUS <tshkhanov@mail.ru>
2026-07-24 12:12:35 +0300

    fix(frontend): configure Axios for FastAPI CSRF headers

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -2,15 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   withCredentials: true,
-});
-
-api.interceptors.request.use((config) => {
-  // Try to get CSRF token from cookies
-  const match = document.cookie.match(/(?:^|;) ?csrf_token=([^;]*)(?:;|$)/);
-  if (match && match[1]) {
-    config.headers['X-CSRF-Token'] = match[1];
-  }
-  return config;
+  xsrfCookieName: 'csrf_token',
+  xsrfHeaderName: 'X-CSRF-Token',
 });
 
 export default api;
